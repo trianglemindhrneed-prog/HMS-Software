8aff6a22b369578613dde53fa192062903e3ad81
Trianglemind PC <trianglemind@outlook.com>
2026-01-30 16:38:39 +0530

    commit by shivam search slot

---
+++ Areas/Admin/Models/DoctorSlot.cs
@@ -0,0 +1,20 @@
+﻿namespace HMSCore.Areas.Admin.Models
+{
+ 
+        public class DoctorSlot
+        {
+            public int DoctorId { get; set; }
+            public int ScheduleId { get; set; }
+            public string DepartmentName { get; set; }
+            public string FullName { get; set; }
+            public DateTime ScheduleDate { get; set; }
+            public string StartTime { get; set; }
+            public string EndTime { get; set; }
+            public string SlotDuration { get; set; }
+            public string SessionType { get; set; }
+            public string SlotTime { get; set; }
+            public bool IsBlocked { get; set; }
+        }
+     
+
+}
---
+++ Areas/Admin/Models/DoctorSlotViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace HMSCore.Areas.Admin.Models
+{
+    public class DoctorSlotViewModel
+    {
+        public int PageSize { get; set; } = 20;
+        public string FilterColumn { get; set; }
+        public string Keyword { get; set; }
+
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+
+        public int? SelectedDepartmentId
+        {
+            get => DepartmentId;
+            set => DepartmentId = value;
+        }
+
+        public int? SelectedDoctorId
+        {
+            get => DoctorId;
+            set => DoctorId = value;
+        }
+
+        public int? DepartmentId { get; set; }
+        public int? DoctorId { get; set; }
+
+        // CHANGE: string instead of bool
+        public string ShowMorning { get; set; } = "true";
+        public string ShowAfternoon { get; set; } = "true";
+        public string ShowEvening { get; set; } = "true";
+
+        // Helper properties to read as bool in code
+        public bool IsMorning => ShowMorning == "true";
+        public bool IsAfternoon => ShowAfternoon == "true";
+        public bool IsEvening => ShowEvening == "true";
+
+        public List<DoctorSlot> Slots { get; set; } = new List<DoctorSlot>();
+        public List<Department> Departments { get; set; } = new List<Department>();
+        public List<Doctor> Doctors { get; set; } = new List<Doctor>();
+    }
+}
