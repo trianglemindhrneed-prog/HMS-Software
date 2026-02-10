057ef9a427f722e884675975c7c084a368b4ccde
Priyanka Patel <rkspriyapatel@gmail.com>
2026-02-10 18:18:34 +0530

    COMMIT BY PRIYANKA-10FEVEVENING

---
+++ Areas/Admin/Models/AddIPDCheckupViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HMSCore.Areas.Admin.Models
+{
+    public class AddIPDCheckupViewModel
+    {
+        public string? PatientId { get; set; }
+
+        public int? SelectedDoctorId { get; set; }
+        public string? Symptoms { get; set; }
+        public string? Diagnosis { get; set; }
+        public DateTime? CheckupDate { get; set; }
+        public string? ExtraNotes { get; set; }
+
+        public List<Doctor> Doctors { get; set; } = new();
+        public List<MedicineVM> Medicines { get; set; } = new();
+        public List<PrescriptionVM> Prescriptions { get; set; } = new();
+    }
+}
---
+++ Areas/Admin/Models/IPDCheckupHistoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace HMSCore.Areas.Admin.Models
+{
+    public class IPDCheckupHistoryViewModel
+    {
+        public string PatientId { get; set; }
+
+        // Patient Profile
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string BloodGroup { get; set; }
+        public string Age { get; set; }
+        public string Number { get; set; }
+        //public string Address { get; set; }
+        public string ProfilePath { get; set; }
+
+        public List<CheckupVM> Checkups { get; set; } = new();
+    }
+}
