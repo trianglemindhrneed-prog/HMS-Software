4c5c68f530dab79337a55d067868829bac6d09ce
Priyanka Patel <rkspriyapatel@gmail.com>
2026-02-11 18:32:48 +0530

    Commit by Priyanka-11feb

--- Areas/Admin/Models/IPDVital.cs
+++ Areas/Admin/Models/IPDVital.cs
@@ -4,8 +4,7 @@
     {
         public int VitalsId { get; set; }
         public string PatientId { get; set; }
-
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public DateTime RecordedDate { get; set; }
         public string BloodGroup { get; set; }
         public string Temperature { get; set; }
@@ -14,14 +13,9 @@
         public string Weight { get; set; }
         public string BP { get; set; }
         public string SpO2 { get; set; }
-      
-        public string Notes { get; set; }
         public string RR { get; set; }
-      
+        public string Notes { get; set; }
         public string RecordedBy { get; set; }
-
-        //public DateTime? FromDate { get; set; }
-        //public DateTime? ToDate { get; set; }
 
     }
   
